cac4220da908506f4dacda93097e68d91c135a81
Fran Aliss <franalissar@gmail.com>
2022-04-07 11:11:56 -0400

    integrate

--- src/CCadenas.test.js
+++ src/CCadenas.test.js
@@ -29,9 +29,9 @@ describe("Suma de la cadena", () => {
     expect(sumarCadena("[/][<<<]1/2<<<3-4")).toEqual(10);
   });
   it("[/][<<<]1/2<<<3-4<<<1001", () => {
-    expect(sumarCadena("[/][<<<]1/2<<<3-4<<<1131")).toEqual(10);
+    expect(sumarCadena("[/][<<<]1/2<<<3-4<<<1001")).toEqual(10);
   });
-  it("[/][<<<]1/2<<<3-4<<<1001", () => {
+  it("[/][<<<]1/2<<<3-4<<<10", () => {
     expect(sumarCadena("[/][<<<]1/2<<<3-4<<<10")).toEqual(20);
   });
 });
